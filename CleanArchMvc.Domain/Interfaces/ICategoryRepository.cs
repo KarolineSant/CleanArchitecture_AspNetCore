27a3c7dd3db052248dcafb6253201fc71e4b8631
unknown <karolinesantos1296@gmail.com>
2022-06-06 19:04:31 -0300

    my first commit

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    interface ICategoryRepository
+    {
+        // Não tem implementação, são apenas assinatura dos métodos, que serão implementados na classe concreta.
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsyc(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(Product product);
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        // Não tem implementação, são apenas assinatura dos métodos, que serão implementados na classe concreta.
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+
+    }
+}
