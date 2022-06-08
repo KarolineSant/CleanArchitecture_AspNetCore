60807eda6aa0a2bd02e96d032f53d4e97acd7115
unknown <karolinesantos1296@gmail.com>
2022-06-08 10:47:00 -0300

    implantando a camada Application

---
+++ CleanArchMvc.Application/Intarfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Intarfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDTO);
+        Task Update(CategoryDTO categoryDTO);
+        Task Remove(int? id);
+    }
+}
